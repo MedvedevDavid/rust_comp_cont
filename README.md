f171314726500f79ce2b4995e5fc3b27a5cc4404
MedveDev <medvedevdavid@gmail.com>
2023-07-21 09:46:39 +0200

    docker compose is added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Compilation command for raspberry pi:
 ./rust.sh compile armv7-unknown-linux-gnueabihf
+scp rustenv pipc@192.168.0.120:/home/pipc/bins
--- rust.sh
+++ rust.sh
@@ -1,16 +1,16 @@
  #!/bin/bash
 
-if [ $1 == "build" ]
+if [ $1 == "run" ]
 then
-    docker build -t rust_compiler .
-elif [ $1 == "run" ]
-then
-    docker build -t rust_compiler .
-    docker run -it --rm --name rust_compiler -v $(pwd):/usr/src/rustenv rust_compiler
+    docker-compose run rust_compiler
 elif [ $1 == "compile" ]
 then
     cd rustenv/
     cargo build --target=$2
+
+elif [ $1 == "clean" ]
+then
+    docker system prune
 else
     echo "Invalid argument"
 fi
